8823a39edb5397cff9f95df5f1c0bf3fe289fcc9
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-09-19 17:51:15 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { sum } from "./modules/sum";
+
+const btn = document.getElementById("btn_sum");
+
+btn.addEventListener("click", calculate);
+
+function calculate() {
+  const a = input_a.value;
+  const b = input_b.value;
+  const result = sum(a, b);
+  console.log("result =", result);
+  result_id.textContent = result;
+}
